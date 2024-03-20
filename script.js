c897f62c9ef945b2dd4f44d5d75ac87a5487b3f4
Sheraz Hasan <sheraz.ayman@gmail.com>
2024-03-20 07:14:57 +0300

    JS todo list

---
+++ script.js
@@ -0,0 +1,58 @@
+//Variables
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+// Function to create a task
+function createTask(taskText) {
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = taskText;
+    task.appendChild(li);
+
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let editButton = document.createElement('button');
+    editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    editButton.classList.add('editTask');
+    task.appendChild(editButton);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if (taskText === "") {
+        alert('Please enter a task');
+    } else {
+        taskContainer.appendChild(task); //to show the task on screen
+    }
+
+    inputTask.value = ''; //to erase the value after entering it
+
+    checkButton.addEventListener('click', function () {
+        checkButton.parentElement.style.textDecoration = "line-through";
+    });
+
+    deleteButton.addEventListener('click', function () {
+        deleteButton.parentElement.remove();
+    });
+
+    editButton.addEventListener('click', function () {
+        const currentText = li.innerText;
+        const newText = prompt('Edit task:', currentText);
+        if (newText !== null) {
+            li.innerText = newText;
+        }
+    });
+}
+
+//Event listener for add btn
+addTask.addEventListener('click', function () {
+    createTask(inputTask.value);
+});
